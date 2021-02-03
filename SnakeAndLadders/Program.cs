d1690c17b4ce73a4828ef5add2036171b8cf315e
Dipesh Walte <dipesh4edutopia@users.noreply.github.com>
2021-02-03 18:55:21 +0530

    [Dipesh] modify AddedOptionLogic

--- SnakeAndLadders/Program.cs
+++ SnakeAndLadders/Program.cs
@@ -4,14 +4,39 @@ namespace SnakeAndLadders
 {
     class Program
     {
-
+        //static constants
+        const int NO_PLAY = 0;
+        const int LADDER = 1;
+        const int SNAKE = 2;
         static void Main(string[] args)
         {
             int position = 0;
             Random rand = new Random();
             int diceThrow = rand.Next(1, 7);
-            Console.WriteLine(diceThrow);
-            Console.WriteLine("Player 1 Started at Position : " + position);
-        }
+			int option = rand.Next(2);
+			switch (option)
+			{
+				case NO_PLAY:
+
+                    Console.WriteLine("Player Remained at same position : "+position);
+					break;
+
+				case LADDER:
+
+                    position += diceThrow;
+                    Console.WriteLine("Player moved to : "+ position);
+					break;
+
+				case SNAKE:
+
+                    position -= diceThrow;
+                    Console.WriteLine("Player moved to : "+ position);
+					break;
+
+				default:
+					break;
+
+			}//end switch
+		}
     }
 }
